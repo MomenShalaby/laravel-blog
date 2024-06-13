a19e4239f92cceb3b684f412123e046ebcb29c8b
jaxybaxy <momen.ahmed_2010@yahoo.com>
2024-06-13 13:36:15 +0300

    add: Posts migrations, models and relationships

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $userIds = User::pluck('id')->toArray();
+
+
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->sentence(4),
+            'image' => "http://localhost:8000/post_placeholder.png",
+            'user_id' => fake()->randomElement($userIds),
+        ];
+    }
+}
